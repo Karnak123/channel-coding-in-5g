a9fa8f48897c7a6ffbbf7230bfae197d8b0f0be8
Sagnik Mazumder <sagnikmazumdar37@gmail.com>
2022-03-19 14:29:26 +0530

    added code for block concatenation

---
+++ encoder/cbc.jl
@@ -0,0 +1,18 @@
+module CBC
+    export code_block_concatenator
+
+    function code_block_concatenator(a, b, E)
+        if typeof(a) == typeof([1 2])
+            a = vec(a)
+        end
+        if typeof(b) == typeof([1 2])
+            b = vec(b)
+        end
+
+        append!(a, b)
+        if length(a) == 2 * E + 1 
+            append!(a, 0)
+        end
+
+        return a
+end
